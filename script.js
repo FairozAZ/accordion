76562414b2e921ebe129067d8948c65622fcdcf1
fairoz <abdulazizfairoz9@gmail.com>
2023-06-10 23:02:57 +0600

    accordion using javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+let acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.height) {
+      panel.style.height = null;
+    } else {
+      panel.style.height = '80px';
+    } 
+  });
+}
